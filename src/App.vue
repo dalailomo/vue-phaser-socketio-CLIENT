55ee2d9b772e11a2db9171f058ea408f5465517c
DalaiLomo <reisereise.85@gmail.com>
2022-09-11 14:22:40 +0200

    Mount game instance in a div and create an event log that can be emitted to all connected users

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,31 @@
 <template>
     <NamePrompt v-if="showNamePrompt" />
-    <template v-else>
+    <template v-else-if="ready">
         <OnlineUsers />
+        <EventLog />
+        <div class="game-view" :id="containerId" />
     </template>
+    <div v-else>Loading...</div>
 </template>
 
 <script lang="ts">
 import { defineComponent, provide, ref } from "vue";
+import { Game } from "phaser";
 import { connect } from "../modules/socket";
 import OnlineUsers from "./components/OnlineUsers.vue";
+import EventLog from "./components/EventLog.vue";
 import NamePrompt from "./components/NamePrompt.vue";
 import { HANDLE_USER_CONNECTIONS_EVENT } from '../vue-phaser-socketio-SERVER/modules/socket-handlers/handle-user-connections';
+import { HANDLE_IO_EMIT_LOG_EVENT } from '../vue-phaser-socketio-SERVER/modules/socket-handlers/handle-io-emit-log'
+import { launch } from '../vue-phaser-socketio-GAME/index';
+import { PLAY_SCENE_EVENT } from '../vue-phaser-socketio-GAME/scenes/play-scene';
 
 export default defineComponent({
     name: "App",
 
     components: {
         OnlineUsers,
+        EventLog,
         NamePrompt
     },
 
@@ -32,10 +41,29 @@ export default defineComponent({
         const socket = connect("http://localhost:3000", localStorage.getItem("name") || `Unnamed.${Math.random() * 10}`);
         provide("socket", socket);
 
+        // Provide phaser game instance once is ready
+        const containerId = "game-container";
+        const gameInstance: Game = launch(containerId);
+        provide("gameInstance", gameInstance);
+
+        // Let template know when game instance is ready
+        const ready = ref(false);
+        gameInstance.events.on('ready', () => {
+            ready.value = true;
+        });
+
         // Emits the request for connected users
         socket.emit(HANDLE_USER_CONNECTIONS_EVENT.CE_ConnectedUsers);
 
-        return { showNamePrompt }
+        // Emits an event to the server to notify all users that a game object has been clicked
+        gameInstance.events.on(
+            PLAY_SCENE_EVENT.gameEmitsClickedBomb,
+            (payload: string) => {
+                socket.emit(HANDLE_IO_EMIT_LOG_EVENT.CE_LogMessage, payload);
+            }
+        );
+
+        return { containerId, ready, showNamePrompt }
     },
 });
 </script>
---
+++ src/components/EventLog.vue
@@ -0,0 +1,41 @@
+<template>
+    <pre>
+        <li v-for="(message, i) in messages" :key="i">{{ message }}</li>
+    </pre>
+</template>
+
+<script lang="ts">
+import { Socket } from 'socket.io-client';
+import { defineComponent, inject, Ref, ref } from 'vue'
+import { HANDLE_IO_EMIT_LOG_EVENT } from '../../vue-phaser-socketio-SERVER/modules/socket-handlers/handle-io-emit-log'
+
+export default defineComponent({
+    setup() {
+        const socket = inject("socket") as Socket;
+        const messages: Ref<string[]> = ref([]);
+
+        socket.on(HANDLE_IO_EMIT_LOG_EVENT.SE_LogMessage, (payload) => {
+            messages.value.push(`Someone clicked on: ${payload} at ${new Date()}`)
+        });
+
+        return { messages }
+    },
+})
+</script>
+
+<style lang="scss" scoped>
+pre {
+    position: absolute;
+    bottom: 0;
+    overflow-y: auto;
+    left: 0;
+    width: 100%;
+    height: 80px;
+    color: white;
+    background: rgba(0,0,0,.3);
+
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+}
+</style>
